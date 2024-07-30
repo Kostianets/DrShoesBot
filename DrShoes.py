8971a34f8936b02edc07c54076879181a242ba08
Vacur2 <akostyanets@gmail.com>
2024-07-30 18:58:18 +0200

    Global change InlineKeyboardButton to KeyboardButton

--- DrShoes.py
+++ DrShoes.py
@@ -1,87 +1,79 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CallbackContext, CommandHandler, CallbackQueryHandler 
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, WebAppData
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 import pandas as pd
 
 TOKEN = '5972314664:AAFt3BimlGFnHrF-wmtXcb1S7lAzTfaTOCo'
 #TODO: replace ID
 MANAGER = "Pudgesf" 
+#TODO: rename file
+file = pd.read_excel('file.xlsx')
 
 updater = Updater(TOKEN)
 
-def start(update: Update, context: CallbackContext):
+def welcome_message(update: Update, context: CallbackContext):
+    update.message.reply_text('Вітаємо в боті DrShoes!', reply_markup=main_menu())
+
+def main_menu():
     keyboard = [
-        [InlineKeyboardButton("Послуги", callback_data='services')], #InlineKeyboardButton('Ціни', callback_data='prices')],
-        [InlineKeyboardButton("Поширені запитання", callback_data='questions')],
-        [InlineKeyboardButton("Налаштування", callback_data='settings')],
-        [InlineKeyboardButton("Зв'язатися з менеджером", url=f"https://t.me/{MANAGER}")],
-        [InlineKeyboardButton("Перевірити статус замовлення", callback_data='status')]
+        [KeyboardButton("Послуги"), KeyboardButton("Поширені запитання")],
+        [KeyboardButton("Налаштування"), KeyboardButton("Перевірити статус замовлення")],
+        [KeyboardButton("Зв'язатися з менеджером")]
     ]
     
-    reply_keyboard = InlineKeyboardMarkup(keyboard)
-    
-    update.message.reply_text('Вітаємо в боті DrShoes!', reply_markup=reply_keyboard)
-    
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
 def button(update: Update, context: CallbackContext):
-    query = update.callback_query
-    query.answer()
+    text = update.message.text
+    # chat_id = update.message.chat_id
+    # message_id = update.message.message_id
     
-    if query.data == 'services':
-        keyboard = [
-            [InlineKeyboardButton("Послуга 1", callback_data='service 1'), InlineKeyboardButton("Послуга 2", callback_data='service 2')],
-            [InlineKeyboardButton("Послуга 3", callback_data='service 3'), InlineKeyboardButton("Послуга 4", callback_data='service 4')],
-            [InlineKeyboardButton("<< Поверутися назад", callback_data='return1')]
-        ]
-        
-        reply_keyboard = InlineKeyboardMarkup(keyboard)
+    match text:
+        case "Послуги":
+            update.message.reply_text('Наші послуги:', reply_markup=services())
+        case "Налаштування":
+            update.message.reply_text('Налаштування:', reply_markup=settings())
+        case "Поширені запитання":
+            update.message.reply_text("Запитання:", reply_markup=faq())
+        case "Зв'язатися з менеджером":
+            update.message.reply_text(f"З менеджером можете зв'язатися за цим посиланням: @{MANAGER}")
+        case "<< Повернутися назад":
+            update.message.reply_text('Вітаємо в боті DrShoes!', reply_markup=main_menu())
+        case _:
+            update.message.reply_text("Будь ласка, оберіть один з варіантів:", reply_markup=main_menu())
 
-        query.edit_message_text('Тестове повідомлення', reply_markup=reply_keyboard)
+def services():
+    keyboard = [
+        [KeyboardButton("Послуга 1"), KeyboardButton("Послуга 2")],
+        [KeyboardButton("Послуга 3"), KeyboardButton("Послуга 4")],
+        [KeyboardButton("<< Повернутися назад")]
+    ]
     
-    if query.data == 'questions':
-        keyboard = [
-            [InlineKeyboardButton("Запитання 1", callback_data='question 1'), InlineKeyboardButton("Запитання 2", callback_data='question 2')],
-            [InlineKeyboardButton("Запитання 3", callback_data='question 3'), InlineKeyboardButton("Запитання 4", callback_data='question 4')],
-            [InlineKeyboardButton("<< Поверутися назад", callback_data='return1')]
-        ]
-        
-        reply_keyboard = InlineKeyboardMarkup(keyboard)
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-        query.edit_message_text('Тестове повідомлення', reply_markup=reply_keyboard)
-        
-    if query.data == 'settings':
-        keyboard = [
-            [InlineKeyboardButton('Мова', callback_data='lang')],
-            [InlineKeyboardButton('<< Повернутися назад', callback_data='return1')]
-        ]
-        
-        reply_keyboard = InlineKeyboardMarkup(keyboard)
-
-        query.edit_message_text('Тестове повідомлення', reply_markup=reply_keyboard)
+def settings():
+    keyboard = [
+        [KeyboardButton("Мова")],
+        [KeyboardButton("<< Повернутися назад")]
+    ]
     
-    if query.data == 'status':
-        keyboard = [
-            [InlineKeyboardButton('<< Повернутися назад', callback_data='return1')]
-        ]
-        
-        query.edit_message_caption('Введіть номер замовлення:', reply_markup=reply_keyboard)
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+def faq():
+    keyboard = [
+        [KeyboardButton("Запитання 1"), KeyboardButton("Запитання 2")],
+        [KeyboardButton("Запитання 3"), KeyboardButton("Запитання 4")],
+        {KeyboardButton("<< Повернутися назад")}
+    ]
     
-    if query.data == 'return1':
-        keyboard = [
-            [InlineKeyboardButton("Послуги", callback_data='services')], #InlineKeyboardButton('Ціни', callback_data='prices')],
-            [InlineKeyboardButton("Поширені запитання", callback_data='questions')],
-            [InlineKeyboardButton("Налаштування", callback_data='settings')],
-            [InlineKeyboardButton("Зв'язатися з менеджером", url=f"https://t.me/{MANAGER}")]
-        ]
-
-        reply_keyboard = InlineKeyboardMarkup(keyboard)
-
-        query.edit_message_text('Вітаємо в боті DrShoes!', reply_markup=reply_keyboard)
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 def main():
     #update_queue = None
     dispatcher = updater.dispatcher
     
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CallbackQueryHandler(button))
+    dispatcher.add_handler(CommandHandler('start', welcome_message))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, button))
+
     
     updater.start_polling()
     updater.idle()
